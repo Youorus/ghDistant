d27fe6aa10ce9152ebfec04542893e383d47c1b0
Marc <mtakoumba@gmail.com>
2025-05-20 14:35:34 +0200

    resolution des conflit

--- Helloworld.algo.py
+++ Helloworld.algo.py
@@ -7,13 +7,9 @@ def fonction_principale():
     # Création de la fenêtre
     fenetre = tk.Tk()
     fenetre.title(HELLO_WORLD_TITLE)
-<<<<<<< HEAD
     fenetre.geometry("400x400")
-
-=======
-    fenetre.geometry("200x200")
     fenetre.configure(bg="green")  # Fond d’écran vert
->>>>>>> 96cd0cf (C6 : couleur de fond en vert (fix_C5))
+
     
     # Création du label de texte
     label = tk.Label(fenetre, text=HELLO_WORLD_TITLE, fg="yellow", bg="black")
