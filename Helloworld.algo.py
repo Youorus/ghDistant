8716ec4ad99663dcdebd3ab746be2955db4dfc7d
Youorus2 <tdsparis15@gmail.com>
2025-05-21 12:19:48 +0200

    Close #4

--- Helloworld.algo.py
+++ Helloworld.algo.py
@@ -2,17 +2,18 @@
 
 import tkinter as tk
 from constantes import HELLO_WORLD_TITLE, HELLO_WORLD_TEXT
+from datetime import date
 
 def fonction_principale():
     # Création de la fenêtre
     fenetre = tk.Tk()
     fenetre.title(HELLO_WORLD_TITLE)
-    fenetre.geometry("400x400")
+    fenetre.geometry("500x300")
     fenetre.configure(bg="cyan")  # Fond d’écran vert
 
     
     # Création du label de texte
-    label = tk.Label(fenetre, text=HELLO_WORLD_TITLE, fg="green", bg="black")
+    label = tk.Label(fenetre, text=HELLO_WORLD_TITLE + str(date.today()), fg="green", bg="black")
     label.pack(expand=True)  # Centrage vertical et horizontal
     
     # Finalisation
